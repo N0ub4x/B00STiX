9c6f8eb6862026ed95b96884adca038a539efe81
N0ub4x <85786075+N0ub4x@users.noreply.github.com>
2021-06-12 11:32:06 +0200

    Add files via upload

---
+++ RBX Clean.bat
@@ -0,0 +1,6 @@
+title If there is an roblox update execute this
+cd C:\Users\Brian\AppData\Local\Roblox\Versions\version-7d96d7dad25d49f1\PlatformContent\pc\textures
+cls
+del aluminum brick cobblestone concrete diamondplate fabric glass granite grass ice marble metal pebble plastic rust sand slate water wood woodplanks
+del C:\Windows\Temp
+del C:\Users\Brian\AppData\Local\Temp
